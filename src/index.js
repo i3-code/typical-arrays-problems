8bd9baddb40178ef379c193b1e8081f1de116c4c
i3-code <i3pobox@gmail.com>
2020-09-10 08:28:39 +0300

    feat: add Typical Arrays Problems solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (!array) return 0;
+  let res = array.sort(function(a, b) { return a - b; });
+  return res[0] || 0;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (!array) return 0;
+  let res = array.sort(function(a, b) { return a - b; });
+  return res.reverse()[0] || 0;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (!array) return 0;
+  let sum = array.reduce(function(a, b) { return a + b; }, 0);
+  return (sum / array.length) || 0;
 }
